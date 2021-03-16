5956c68c4d56aa3fe4bc0c4bd6c6fb06fa82ef03
carlos-osorio-developer <osorio.uis@gmail.com>
2021-03-16 15:16:09 -0500

    Add bubble_sort_by method

--- algorythm.rb
+++ algorythm.rb
@@ -16,4 +16,28 @@ def bubble_sort(nonsorted)
     return nonsorted
 end
 
+def bubble_sort_by(array)
+    sorted=false
+    puts "Please enter a non empty array" if array.empty?
+    until sorted
+        0.upto(array.length - 2) do |i| 
+            change=0
+            checker=yield(array[i],array[i+1])
+            if checker>0
+                array[i], array[i+1]=array[i+1], array[i]
+                change+=1                      
+            end
+            if change==0
+                sorted=true
+            end
+        end
+    end
+end
+
+p "Sorted_by array: #{bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end}"
+
+
 p "Sorted array: #{bubble_sort([5,5,6,2,1])}"
+
