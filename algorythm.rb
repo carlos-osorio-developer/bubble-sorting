77609972929cf59d517a7e13c9e86bc1feb2d671
JAAR96 <jaarriagaramos@gmail.com>
2021-03-16 15:22:16 -0600

    fix bug with Redundant return on file alrorythm.rb

--- algorythm.rb
+++ algorythm.rb
@@ -1,15 +1,15 @@
 def bubble_sort(nonsorted)
   i = 0
-  while i < nonsorted.length-1 do
+  while i < nonsorted.length - 1
     j = 0
     while j < nonsorted.length - 1
-      check = nonsorted[j].to_i - nonsorted[j+1].to_i
-      nonsorted[j], nonsorted[j+1] = nonsorted[j + 1], nonsorted[j] if check.positive?
+      check = nonsorted[j].to_i - nonsorted[j + 1].to_i
+      nonsorted[j], nonsorted[j + 1] = nonsorted[j + 1], nonsorted[j] if check.positive?
       j += 1
     end
     i += 1
   end
-  return nonsorted
+  nonsorted
 end
 
 def bubble_sort_by(array)
@@ -17,15 +17,15 @@ def bubble_sort_by(array)
   until sorted
     0.upto(array.length - 2) do |i|
       change = 0
-      checker = yield(array[i],array[i + 1])
+      checker = yield(array[i], array[i + 1])
       array[i], array[i + 1] = array[i + 1], array[i] if checker.positive?
       change += 1 if checker.positive?
-      sorted=true if change.zero?
+      sorted = true if change.zero?
     end
   end
-  return array
+  array
 end
 
-puts "Sorted by array: #{bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length} }"
+puts "Sorted by array: #{bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }}"
 
 puts "Sorted array: #{bubble_sort([5, 5, 6, 2, 1])}"
