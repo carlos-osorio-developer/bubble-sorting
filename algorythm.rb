0938befb9199dde2b357a244d2fae47d78164276
JAAR96 <jaarriagaramos@gmail.com>
2021-03-16 15:16:17 -0600

    fix code to match linter requirements

--- algorythm.rb
+++ algorythm.rb
@@ -1,39 +1,31 @@
 def bubble_sort(nonsorted)
-    i=0
-    while  i<nonsorted.length-1 do
-        j=0
-        while j<nonsorted.length-1 do
-            if nonsorted[j].to_i>nonsorted[j+1].to_i
-                a=nonsorted[j]
-                nonsorted[j]=nonsorted[j+1]
-                nonsorted[j+1]=a
-            end
-            j+=1
-        end
-        i+=1
+  i = 0
+  while i < nonsorted.length-1 do
+    j = 0
+    while j < nonsorted.length - 1
+      check = nonsorted[j].to_i - nonsorted[j+1].to_i
+      nonsorted[j], nonsorted[j+1] = nonsorted[j + 1], nonsorted[j] if check.positive?
+      j += 1
     end
-    return nonsorted
+    i += 1
+  end
+  return nonsorted
 end
 
 def bubble_sort_by(array)
-    sorted=false
-    return puts "Please enter a non empty array" if array.empty? 
-    until sorted
-        0.upto(array.length - 2) do |i| 
-            change=0
-            checker=yield(array[i],array[i+1])
-            if checker>0
-                array[i], array[i+1]=array[i+1], array[i]
-                change+=1                      
-            end
-            if change==0
-                sorted=true
-            end
-        end
+  sorted = false
+  until sorted
+    0.upto(array.length - 2) do |i|
+      change = 0
+      checker = yield(array[i],array[i + 1])
+      array[i], array[i + 1] = array[i + 1], array[i] if checker.positive?
+      change += 1 if checker.positive?
+      sorted=true if change.zero?
     end
-    return array
+  end
+  return array
 end
 
-puts "Sorted by array: #{bubble_sort_by(["hi","hello","hey"]){|left,right|left.length - right.length}}"
+puts "Sorted by array: #{bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length} }"
 
-puts "Sorted array: #{bubble_sort([5,5,6,2,1])}"
+puts "Sorted array: #{bubble_sort([5, 5, 6, 2, 1])}"
